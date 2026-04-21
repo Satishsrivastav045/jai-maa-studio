f779a1a17d3995252bf81162eee7949d07c6bb65
Satishsrivastav045 <satishsrivasatav339@gmail.com>
2026-04-21 13:28:48 +0530

    fix admin login and remove apps.py hack

--- main/apps.py
+++ main/apps.py
@@ -3,24 +3,3 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
-
-    def ready(self):
-        import os
-
-        # ⚠️ duplicate run avoid
-        if os.environ.get('RUN_MAIN') != 'true':
-            return
-
-        try:
-            from django.contrib.auth import get_user_model
-            User = get_user_model()
-
-            # 🔥 admin reset
-            User.objects.filter(username="admin").delete()
-
-            User.objects.create_superuser("admin", "admin@gmail.com", "admin123")
-
-            print("Superuser reset done")
-
-        except Exception as e:
-            print(e)
